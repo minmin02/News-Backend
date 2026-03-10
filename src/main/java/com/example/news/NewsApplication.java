bd39dbef16443985aa0172d82b7f5d694d3516af
minmin02 <aktr378@gmail.com>
2026-03-10 19:52:11 +0900

    fix :  dockerHub Token 재발급

--- src/main/java/com/example/news/NewsApplication.java
+++ src/main/java/com/example/news/NewsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NewsApplication {
 
+	//수정
 	public static void main(String[] args) {
 		SpringApplication.run(NewsApplication.class, args);
 	}
