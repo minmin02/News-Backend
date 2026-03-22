73af97e2fd02496b18dd3d06ffe0cdd90083240a
김민규 <gimmingyu@gimmingyuui-MacBookAir-2.local>
2026-03-22 17:37:58 +0900

    feat: 로컬 개발 환경 최적화 및 도커 컴포즈 설정 분리

--- src/main/java/com/example/news/NewsApplication.java
+++ src/main/java/com/example/news/NewsApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NewsApplication {
+public class
+
+NewsApplication {
 	//수정
 	public static void main(String[] args) {
 		SpringApplication.run(NewsApplication.class, args);
